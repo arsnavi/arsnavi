d7833ea8070ab937b7b65d72d13dca6679f4567b
fms <keunes411@gmail.com>
2017-12-04 11:30:37 +0900

    a

---
+++ arsnavi/arsnavi/Models/StudentContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace arsnavi.Models
+{
+    //参考:http://surferonwww.info/BlogEngine/post/2014/07/23/mvc4-web-application-created-by-using-the-internet-application-template.aspx
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> students { get; set; }
+    }
+}
