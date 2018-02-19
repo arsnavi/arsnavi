de44456927d7e9913201c4ed5ca7c084d4d2ca32
fms <keunes411@gmail.com>
2018-02-19 12:51:43 +0900

    link to testpage

--- arsnavi/arsnavi/Controllers/LoginController.cs
+++ arsnavi/arsnavi/Controllers/LoginController.cs
@@ -7,13 +7,31 @@ using System.Web;
 using System.Web.Mvc;
 using arsnavi.Models;
 
+using arsnavi.Services;
+
 namespace arsnavi.Controllers
 {
     public class LoginController : Controller
     {
-        public ActionResult Login()
+
+        private DatabaseService _service = new DatabaseService();
+
+        public ActionResult Index()
         {
             return View();
+        }
+
+        // ［送信］ボタンをクリックしたときに呼び出される
+        // Postメソッドを定義
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(String name)
+        {
+            ViewData["msg"] = String.Format("Hello, {0}!", name);
+
+            //ログイン処理
+
+            //Resultビューを指定
+            return View("Result");
         }
     }
 }
